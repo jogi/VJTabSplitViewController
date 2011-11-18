04cb20b2d0b8094d1b5277ad31e61f7355c3e63e
Vashishtha Jogi <vashishthajogi@gmail.com>
2011-11-18 11:43:02 -0800

    Initial Commit

---
+++ VJTabSplitViewController/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  VJTabSplitViewController
+//
+//  Created by Vashishtha Jogi on 11/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
